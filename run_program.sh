33a464957598d8e9925b7e13d9a4c20931dbb187
Haifeng Zhao <hf_zhao@yeah.net>
2019-11-16 19:52:55 +0800

    [t2] modify to use multi-gpus to run training.
       * Keys: make sure all data (models and datasets) are in cuda.
       * the cnn model in atten_mask is not in cuda, but in cpu. add this will be ok.
       * done.
       * ref: https://erickguan.me/2019/pytorch-parallel-model

--- run_program.sh
+++ run_program.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 program="src/train.py sketchy_extend"
-param="--epochs 140 --load checkpoint/exp_idf/10_run-batchSize_60/checkpoint.pth  --early_stop 120 --ngpu 1"
+#param="--epochs 140 --load checkpoint/exp_idf/10_run-batchSize_60/checkpoint.pth  --early_stop 120 --ngpu 1"
+param="--batch_size 60 --epochs 140  --early_stop 120 --ngpu 2"
 
 if [ -z $1 ]; then
     debug="-m ptvsd --host 0.0.0.0 --port 5678 --wait"
@@ -10,5 +11,6 @@ else
 fi
 
 echo python $debug $program $param
-python $debug $program $param > run_log-"`date +%Y%m%d_%H%M%S`".txt
+#python $debug $program $param > run_log_parallel-"`date +%Y%m%d_%H%M%S`".txt
+python $debug $program $param 
 
