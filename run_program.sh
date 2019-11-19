02e400a230e8107bd8badb09790b29eaceff7c72
Haifeng Zhao <hf_zhao@yeah.net>
2019-11-19 10:29:14 +0800

    add run parallel mode, max mAP: 0.306

--- run_program.sh
+++ run_program.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 program="src/train.py sketchy_extend"
-#param="--epochs 140 --load checkpoint/exp_idf/10_run-batchSize_60/checkpoint.pth  --early_stop 120 --ngpu 1"
-param="--batch_size 60 --epochs 140  --early_stop 120 --ngpu 2"
+#1 run: param="--batch_size 60 --epochs 50  --early_stop 40 --ngpu 2"
+param="--epochs 120 --load checkpoint/exp_idf/33_run-batchSize_60/checkpoint.pth  --early_stop 100 --ngpu 2"
 
 if [ -z $1 ]; then
     debug="-m ptvsd --host 0.0.0.0 --port 5678 --wait"
@@ -11,6 +11,6 @@ else
 fi
 
 echo python $debug $program $param
-#python $debug $program $param > run_log_parallel-"`date +%Y%m%d_%H%M%S`".txt
-python $debug $program $param 
+python $debug $program $param > run_log_parallel-"`date +%Y%m%d_%H%M%S`".txt
+#python $debug $program $param 
 
