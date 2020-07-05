4c85711a5d854518030052be784bab3e96dfc983
Haifeng Zhao <hf_zhao@yeah.net>
2020-07-05 10:49:37 +0800

    use 120 epchs.

--- run_program.sh
+++ run_program.sh
@@ -2,7 +2,7 @@
 
 program="src/train.py tuberlin_extend"
 #param="--data_path ./datasets/tuberlin-extended-dataset/content --batch_size 60 --epochs 50  --early_stop 40 --atten --ngpu 2"
-param="--data_path ./datasets/tuberlin-extended-dataset/content --epochs 100 --load checkpoint/exp_idf/28_run-batchSize_60/checkpoint.pth  --early_stop 80 --ngpu 2"
+param="--data_path ./datasets/tuberlin-extended-dataset/content --epochs 120 --load checkpoint/exp_idf/31_run-batchSize_60/checkpoint.pth  --early_stop 110 --ngpu 2"
 
 if [ "$1" == "d" ] || [ "$1" == "debug" ]; then
     debug="-m ptvsd --host 0.0.0.0 --port 5678 --wait"
